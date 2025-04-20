9e0ed542e666e14b19f84f73cd6e0ed0b80737e7
li-zhenyu0511 <zjlsjnlzy@163.com>
2025-04-20 15:15:33 +0800

    更新api url

--- src/env.py
+++ src/env.py
@@ -20,8 +20,8 @@ alpha = math.sqrt(2) / 1000.0 # w in the paper
 
 # rag_model_id_or_path = "Alibaba-NLP/gte-Qwen2-1.5B-instruct"
 rag_model_id_or_path = "Qwen/Qwen2-0.5B-Instruct"
-openai_api_key = "sk-qC4pmgaIQuCgMqBROVLYMMv9ZuHDeJQafeZeLfWAqxJQaahO"
-openai_base_url = "https://chatapi.littlewheat.com/v1"
+openai_api_key = "sk-YIkcYQ4PqHhKPyJf7sE8NQALxRwRrix2VXglnXOpkkheP7dY"
+openai_base_url = "https://api.nuwaapi.com/v1"
 default_model = "gpt-4o"
 pg_user = "myuser"
 pg_db = "mydb"
